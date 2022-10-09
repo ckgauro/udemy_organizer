2fd0b5658bbb4c98d2b63cdc5c67f896bf1bc316
Chandra kishor Gauro <ckgauro@gmail.com>
2022-10-09 12:46:54 -0400

    Udemy organizer

---
+++ udemyorganizer/src/main/java/com/gauro/udemyorganizer/UdemyorganizerApplication.java
@@ -0,0 +1,13 @@
+package com.gauro.udemyorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyorganizerApplication.class, args);
+	}
+
+}
---
+++ udemyorganizer/src/test/java/com/gauro/udemyorganizer/UdemyorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.udemyorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
