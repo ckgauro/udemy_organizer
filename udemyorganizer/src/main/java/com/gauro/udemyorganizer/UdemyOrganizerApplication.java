5d3b23d0666e7043f87390f0c588c5b66a8d17ef
Chandra kishor Gauro <ckgauro@gmail.com>
2022-10-09 12:52:28 -0400

    Udemy organizer

--- udemyorganizer/src/main/java/com/gauro/udemyorganizer/UdemyOrganizerApplication.java
+++ udemyorganizer/src/main/java/com/gauro/udemyorganizer/UdemyOrganizerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UdemyorganizerApplication {
+public class UdemyOrganizerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UdemyorganizerApplication.class, args);
+		SpringApplication.run(UdemyOrganizerApplication.class, args);
 	}
 
 }
